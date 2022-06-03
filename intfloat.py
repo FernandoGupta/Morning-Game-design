d8eefb3c639fc5547aec42734581bd94da913773
FernandoGupta <fernandogupta06@gmail.com>
2022-06-03 09:03:40 -0500

    new

--- intfloat.py
+++ intfloat.py
@@ -21,3 +21,8 @@ if(number%5==0):
     print(number,'is a multiple of 5')
 else: 
     print(number,'is not a multiple of 5')
+
+if(number%5==0) and (number%3==0):
+    print(number,'is a multiple of 5 and 3')
+else: print(number,'is not a multiple of 5 and 3')
+    
