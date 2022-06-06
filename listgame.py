9852d6515755f9134de196520817c578170f7381
FernandoGupta <fernandogupta06@gmail.com>
2022-06-06 09:47:06 -0500

    new

--- listgame.py
+++ listgame.py
@@ -14,53 +14,68 @@ seconds=.5
 
 list = ["coral","scallop","sea urchin","oyster","mussel","cockle","clam","geoduck","abelone","ostrea"]
 theword=random.choice(list)
-print("|***************************************|")
-print("|         Guess The Animal!!!           |")
-print("|^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^|")
-print("|          1.Instructions               |")
-print("|   There are 10 animals in this game   |")
-print("|     You must guess one of them        |")
-print("|First we will provide you with one hint|")
-print("|           !Your Hint Is!              |")
-print("|  These animals are big fans of water  |")
-print("|***************************************|")
+Game=True 
+cnt=0
+# a function is a section of the program that we call when we need it 
+def hint():
+    print("|*************************************|")
+    print("|         Here is a new hint          |")
+    print("|These creatures all have a hard shell|")
+    print("|        only 2 shells in fact        |")
+    print("|*************************************|")
 
-guess1=input("plese put your guess here: ")
-if guess1 == theword:
-    print("Congrats, You got it")
-else:
-    print("you missed it, try again")
-
-print("|*************************************|")
-print("|         Here is a new hint          |")
-print("|These creatures all have a hard shell|")
-print("|        only 2 shells in fact        |")
-print("|*************************************|")
-
-guess2 = input("plese put your new guess here: ")
-if guess2 == theword:
-    print("Congrats, You got it")
-else:
-    print("wrong again, you are pretty bad at this, try again")
-
-print("|**************************************|")
-print("|       Here is your final hint        |")
-print("|  These creatures almost never move   |")
-print("|**************************************|")
-
-guess3 = input("plese put your final guess here: ")
-
-if guess3 == theword:
-    print("Congrats, You got it")
-else:
-    print("You are horrible at guessing, no more hints, go till you get it right")
-
-
-while True:
-    ans = input("plese put your guess here: ")
-    if ans == theword:
-         name = True 
-         print("Congrats, You got it")
-         break 
+    guess2 = input("plese put your new guess here: ")
+    if guess2 == theword:
+        print("Congrats, You got it")
     else:
-        print("wrong again, try again")
+        print("wrong again, you are pretty bad at this, try again")
+
+    print("|**************************************|")
+    print("|       Here is your final hint        |")
+    print("|  These creatures almost never move   |")
+    print("|**************************************|")
+
+while Game:
+    print("|***************************************|")
+    print("|         Guess The Animal!!!           |")
+    print("|^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^|")
+    print("|          1.Instructions               |")
+    print("|   There are 10 animals in this game   |")
+    print("|     You must guess one of them        |")
+    print("|First we will provide you with one hint|")
+    print("|           !Your Hint Is!              |")
+    print("|  These animals are big fans of water  |")
+    print("|***************************************|")
+# user name make more persoal and will be used to keep score    
+    name= input('what is your name: ')
+    print(name, end=" ")
+    answer=input("would you like to  play " )
+    if 'n' or 'N' in answer:
+        break
+
+    guess1=input("plese put your guess here: ")
+    if guess1 == theword:
+        print("Congrats, You got it")
+    else:
+        print("you missed it, try again")
+
+
+
+    guess3 = input("plese put your final guess here: ")
+
+    if guess3 == theword:
+        print("Congrats, You got it")
+    else:
+        print("You are horrible at guessing, no more hints, go till you get it right")
+
+
+    # Give a Variable to your loop 
+    check=True 
+    while check:
+        ans = input("plese put your guess here: ")
+        if ans == theword:
+            name = True 
+            print("Congrats, You got it")
+            check=False 
+        else:
+            print("wrong again, try again")
